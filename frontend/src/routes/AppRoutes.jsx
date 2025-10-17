f540b7b26abf7dede2c1e91645ac5fa6b6b1557c
EnzoAyala <rickayalatarazona@gmail.com>
2025-10-16 20:35:13 -0500

    properties y cambio menor en approute

--- frontend/src/routes/AppRoutes.jsx
+++ frontend/src/routes/AppRoutes.jsx
@@ -31,7 +31,7 @@ const AppRoutes = () => (
       }
     />
     <Route path="/proyectos/:proyectoId/tareas" element={<TareasPage />} />
-   <Route
+    <Route
       path="/proyectos"
       element={
         <ProtectedRoute requiredRole="ROLE_USER">
