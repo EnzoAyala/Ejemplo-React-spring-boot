cab5c3e896d532b20136ad2c217500711f504de2
EnzoAyala <rickayalatarazona@gmail.com>
2025-12-10 15:35:37 -0500

    intento de arreglo

--- backend/src/main/java/com/aprender/backend/persistence/entity/Message.java
+++ backend/src/main/java/com/aprender/backend/persistence/entity/Message.java
@@ -20,7 +20,7 @@ public class Message {
 
     // Contenido cifrado del mensaje (AES). Guardado como Base64 (IV + ciphertext)
     @Lob
-    @Column(name = "contenido", nullable = false)
+    @Column(name = "contenido", nullable = false, columnDefinition = "TEXT")
     private String contenido;
 
     // Fecha/hora del mensaje
